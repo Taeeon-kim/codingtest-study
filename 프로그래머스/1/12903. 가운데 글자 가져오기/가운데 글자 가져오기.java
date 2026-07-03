7665a8be4c5e1e568a7539e8191a563444911f9c
김태언 <58901635+Taeeon-kim@users.noreply.github.com>
2026-07-03 10:44:00 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 3.80 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String s) {
+        
+        if(s.length() % 2 == 0) return "" + s.charAt(s.length() / 2 - 1) + s.charAt(s.length() / 2);
+        else return "" + s.charAt(s.length() / 2);
+    }
+}
